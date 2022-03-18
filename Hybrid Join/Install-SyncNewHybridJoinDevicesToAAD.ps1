5b01b17abf6cd2476fc34dbed7bdd1ead437e911
silvermarkg <19527097+silvermarkg@users.noreply.github.com>
2022-03-18 23:41:24 +0000

    Updated after testing

--- Hybrid Join/Install-SyncNewHybridJoinDevicesToAAD.ps1
+++ Hybrid Join/Install-SyncNewHybridJoinDevicesToAAD.ps1
@@ -29,27 +29,35 @@ $ScriptName = "SyncNewHybridJoinDevicesToAAD.ps1"
 $SourceScriptPath = "$($PSScriptRoot)\$($ScriptName)"
 $TargetPath = "$($env:ProgramData)\Sync New Hybrid Join Devices"
 $TaskName = "Sync New Hybrid Join Devices"
-$TriggerStartTime = Get-Date -Hour 0 -Minute 0 -Seconds 0
+$TriggerStartTime = Get-Date -Hour 0 -Minute 0 -Second 0
 
 # Create target path if it does not exist
 if (-Not (Test-Path -Path $TargetPath -PathType Container)) {
     Write-Host -Object "Creating folder $($TargetPath)"
-    New-Item -Path $Path -ItemType Directory | Out-Null
+    New-Item -Path $TargetPath -ItemType Directory | Out-Null
 }
 
 # Copy script file
 if (Test-Path -Path $SourceScriptPath -PathType Leaf) {
-    Write-Host -Object "Copying script"
-    Copy-Item -Path $SourceScriptPath -Destination $TargetScriptPath -Force
+    Write-Host -Object "Copying script $($SourceScriptPath)"
+    Copy-Item -Path $SourceScriptPath -Destination $TargetPath -Force
 }
 
 # Create scheduled task
 Write-Host -Object "Creating scheduled task '$($TaskName)'"
-$stAction = New-ScheduledTaskAction -Execute "powershell.exe" -Argument " -ExecutionPolicy Bypass -File ""$($TargetScriptPath)\$($ScriptName)"" -ModifiedTimeMinutes $TaskRepetition"
+$stAction = New-ScheduledTaskAction -Execute "powershell.exe" -Argument " -ExecutionPolicy Bypass -File ""$($TargetPath)\$($ScriptName)"" -ModifiedTimeMinutes $TaskRepetition"
 $stTrigger = New-ScheduledTaskTrigger -Daily -DaysInterval 1 -At $TriggerStartTime
+#$stTrigger.ExecutionTimeLimit = (New-TimeSpan -Minutes 30)
 $stTempTrigger = New-ScheduledTaskTrigger -Once -At $TriggerStartTime -RepetitionDuration (New-TimeSpan -Days 1) -RepetitionInterval (New-TimeSpan -Minutes $TaskRepetition)
+$stTempTrigger.Repetition.StopAtDurationEnd = $false
 $stTrigger.Repetition = $stTempTrigger.Repetition
 $stPrincipal = New-ScheduledTaskPrincipal -UserId "LOCALSERVICE" -LogonType ServiceAccount
-Register-ScheduledTask -TaskName $TaskName -TaskPath "\" -Action $stAction -Trigger $stTrigger -Principal $stPrincipal
+$stSettings = New-ScheduledTaskSettingsSet -StartWhenAvailable -MultipleInstances Parallel -ExecutionTimeLimit (New-TimeSpan -Minutes 30)
+Register-ScheduledTask -TaskName $TaskName -TaskPath "\" -Action $stAction -Trigger $stTrigger -Principal $stPrincipal -Settings $stSettings | Out-Null
 
+# Creating event source
+Write-Host -Object "Creating event source '$($TaskName)"
+New-EventLog -LogName Application -Source $TaskName
+
+Write-Host -Object "Done"
 #endregion Main code
